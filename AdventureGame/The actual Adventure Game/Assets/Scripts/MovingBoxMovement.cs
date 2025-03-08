f5a0da3403eec673bf22af0e871f82113cb67721
Brook7 <10991612@uvu.edu>
2025-03-07 19:22:35 -0700

    Lab 3

---
+++ AdventureGame/The actual Adventure Game/Assets/Scripts/MovingBoxMovement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+	private Rigidbody rb;
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(Vector3.right * 5000);
+	}
+}

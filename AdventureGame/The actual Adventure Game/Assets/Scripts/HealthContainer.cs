b88c457c6fceae484071703985ad6206518e14aa
Brook7 <10991612@uvu.edu>
2025-04-11 18:19:23 -0600

    Lab 7

---
+++ AdventureGame/The actual Adventure Game/Assets/Scripts/HealthContainer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthContainer : MonoBehaviour
+{
+    public SimpleFloatData healthValue;
+
+    public void adjustHealth(float amount)
+    {
+        healthValue.UpdateValue(amount);
+    }
+}

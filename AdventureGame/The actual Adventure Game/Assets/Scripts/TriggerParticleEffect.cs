52c6d9b306c85c9db368846d55917353dc6ef473
Brook7 <10991612@uvu.edu>
2025-04-12 20:12:22 -0600

    Challenge 10

--- AdventureGame/The actual Adventure Game/Assets/Scripts/TriggerParticleEffect.cs
+++ AdventureGame/The actual Adventure Game/Assets/Scripts/TriggerParticleEffect.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
+    private ParticleSystem particleSystem;
     public int particleAmount = 10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delaybetweenEmissions = 0.5f;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<CharacterController>())
         {
-            GetComponent<ParticleSystem>().Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(particleAmount);
+        yield return new WaitForSeconds(delaybetweenEmissions);
+        
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delaybetweenEmissions);
+        
+        particleSystem.Emit(thirdEmissionAmount);
+        yield return new WaitForSeconds(delaybetweenEmissions);
     }
 }
